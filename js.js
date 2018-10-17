b5539912e1545f08835d2a32344664cd8203a529
AntonyZ89 <Antonybiel1@hotmail.com>
2018-10-16 22:45:16 -0300

    Update js.js

--- js.js
+++ js.js
@@ -78,7 +78,7 @@ $(document).ready(function(){
               }
             }
           });
-    }, 500);
+    }, 1500);
 
         $('.exit').click(function(){
             $(this).parent().toggle('slide', { direction: 'right' }, 400);
