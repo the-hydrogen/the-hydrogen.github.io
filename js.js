eb62513466d088e9acfccd4a2abd5772067d3734
AntonyZ89 <Antonybiel1@hotmail.com>
2018-11-05 14:32:19 -0300

    img+

--- js.js
+++ js.js
@@ -188,4 +188,4 @@ if (url == 'imagens') {
 
 $('.qntAudios').html('<i class="fas fa-volume-down"></i> Áudios: <span>'+131+'</span>');
 $('.qntGIF').html('<i class="fas fa-image"></i> GIFs: <span>'+31+'</span>');
-$('.qntImg').html('<i class="fas fa-image"></i> Imagens: <span>'+72+'</span>');
+$('.qntImg').html('<i class="fas fa-image"></i> Imagens: <span>'+73+'</span>');
